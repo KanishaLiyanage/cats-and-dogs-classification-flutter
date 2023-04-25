72f0ed964d64ced3f7feabecfad912b8d468c6a9
KanishaL <73332154+KanishaLiyanage@users.noreply.github.com>
2023-04-25 20:54:06 +0530

    Update landing.dart

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -18,7 +18,7 @@ class LandingScreen extends StatelessWidget {
             ),
             Container(
               decoration: BoxDecoration(
-                color: Colors.red,
+                color: Colors.yellow,
               ),
             ),
             Container(
